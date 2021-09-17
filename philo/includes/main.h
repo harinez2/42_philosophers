3347e72ebb1c10e30f570dea652333d5cf3f9c42
harinez2 <yonishi@student.42tokyo.jp>
2021-09-18 02:36:47 +0900

    changed file structure

---
+++ philo/includes/main.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonishi <yonishi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/18 01:52:42 by yonishi           #+#    #+#             */
+/*   Updated: 2021/09/18 02:36:06 by yonishi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_H
+# define MAIN_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <stdlib.h>
+# include <errno.h>
+# include <limits.h>
+
+# include "constants.h"
+# include "structs.h"
+# include "prototypes.h"
+
+#endif
---
+++ philo/includes/prototypes.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prototypes.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yonishi <yonishi@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/16 19:42:38 by yonishi           #+#    #+#             */
+/*   Updated: 2021/09/18 02:35:12 by yonishi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PROTOTYPES_H
+# define PROTOTYPES_H
+
+// error.c
+int			print_error(int errcode);
+// lib_ft.c
+size_t		ft_strlen(const char *str);
+void		ft_putchars(char *s);
+void		print_num(long d);
+int			ft_atoi(char *s, int *retnum);
+// lib_util.c
+long		get_time(void);
+void		usleep_exact(long time_in_usec);
+// main.c
+int			main(int argc, char **argv);
+// philo.c
+void		print_status(long time, int who, int something);
+void		*philosopher(void *arg);
+// philo_behavior.c
+int			change_status(t_status *s, int i);
+
+#endif
